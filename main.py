f23c0eab7e8dc1f87ef18e50e79be2aa84c4e533
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-04-19 19:53:20 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,12 @@
+def prime_generator(n):
+    for possiblePrime in range(2, n + 1):
+        isPrime = True
+        for num in range(2, int(possiblePrime ** 0.5) + 1):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            yield possiblePrime
+
+for prime in prime_generator(100):
+    print(prime)
